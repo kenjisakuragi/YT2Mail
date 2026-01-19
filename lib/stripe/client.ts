a99f8bb6302c0a119abb599fad813bac5f66e11f
KenjiSakuragi <kokoronodiary@gmail.com>
2026-01-20 01:58:42 +0900

    Fix typescript error in stripe client

--- lib/stripe/client.ts
+++ lib/stripe/client.ts
@@ -2,6 +2,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    // @ts-ignore
     apiVersion: '2025-02-24.acacia', // Or latest supported version
     appInfo: {
         name: 'Starter Story Insights (JP)',
